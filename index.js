1a895367c8356f2f6373375fc4f649fd53e8fae1
Glitch (heavy-magnificent-flax) <none>
2024-05-05 19:35:18 +0000

    🌨️🧶 Updated with Glitch2

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
-const users = [{ username: "Leif Bech", _id: "1" }];
+const users = [{ username: "Leif Bech", _id: "1", exercises: [] }];
 
 app.post("/api/users", (req, res) => {
   const username = req.body.username;
@@ -23,11 +23,25 @@ app.post("/api/users", (req, res) => {
 app.get("/api/users", (req, res) => {
   return res.json(users);
 });
-/*app.post('/api/users/:_id/exercises', (req,res)=>{
-const _id = params.id
-users.find((x)=> x==_id)
-})
-*/
+const finduserbyId = (userId) => {
+  return users.find((user) => user._id === userId);
+};
+app.post("/api/users/:_id/exercises", (req, res) => {
+  const { _id } = req.params;
+  const { description, duration, date } = req.body;
+  const user = finduserbyId(_id);
+  if (!user) {
+    return res.status(404).json({ error: "User not found" });
+  }
+  const newExercise = {
+    description,
+    duration: parseInt(duration),
+    date: date ? new Date(date) : new Date()
+  };
+user.exercises.push(newExercise)
+  return res.json(user)
+});
+
 console.log("hey");
 console.log("yo");
 
